6aab7262e1291bf84449b443e229f7072d46f537
Zub04ek <nastasyz@ukr.net>
2023-05-16 13:05:11 +0300

    move DB_HOST to process.env

--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://nastya:TqIRYn3CaAc4Tc5H@cluster0.6cfkbd9.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((err) => {
     console.log(err.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
