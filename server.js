ba45e0745786e260f2392252867494f5bc705bcf
Zub04ek <nastasyz@ukr.net>
2023-05-16 11:49:20 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://nastya:TqIRYn3CaAc4Tc5H@cluster0.6cfkbd9.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.log(err.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
